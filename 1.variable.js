68f15b0cec8da8b90876951bac7186b8a30f2f0c
pabitra dangi <pabitra-Tech-hue>
2026-06-22 13:53:51 +0545

    add: js variable

---
+++ 1.variable.js
@@ -0,0 +1,26 @@
+console.log("hello world");
+//!dynamic typed, interpreted and single threaded language
+
+
+
+//!variables
+
+//*variable declaration methods
+//? var,const,let
+
+
+//console.log(12);
+
+//var a = 12;
+//console.log(a);
+//var a = "var";
+//a = 56;
+//console.log(a);
+let b = 45;
+//b = 78;
+console.log(b);
+//const c = 67;
+c = 56;
+console.log(c);
+
+//console.log("end"); 
