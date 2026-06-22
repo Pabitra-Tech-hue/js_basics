115443ec143c10f3b87c9658be6acc8f9ef6dab8
pabitra dangi <pabitra-Tech-hue>
2026-06-22 14:01:03 +0545

    add: js datatypes

---
+++ 2.datatypes.js
@@ -0,0 +1,39 @@
+//!datatypes
+
+//?primitive
+//*number
+let a = 134;
+let b = 456;
+//#string
+let c = "single line lekhnu paryo bhaney"
+let d = '';
+
+//?temperal literal->
+let e = 'multiple line lekhna milney'
+let f="john";
+let d='hello world ${2+2}'
+console.log(f);
+//#bollen
+let g=true;
+let g = false;
+
+
+//#null
+let j = null;
+
+
+//#undefined
+let i;
+console.log(i);//
+
+//?difference between undefined and not defined
+console.log(l);//undefined
+console.log(k);//not defined
+
+//?not defined bhaneko error chai aauney
+
+
+//?non primitive
+//#object
+//$array
+//#function
