1dacba730b1097e9bbdc236cac8c5a1e858473a6
pabitra dangi <pabitra-Tech-hue>
2026-06-22 14:05:09 +0545

    add:js control_statement

---
+++ 4.control_statement.js
@@ -0,0 +1,177 @@
+//control statement
+//#conditinals
+//*if
+// let age=18;
+// if (age>=18){
+    // console.log("can vote");
+// }elese{
+// age>=18;
+// age>=18;
+// age<18->MutationObserver
+// if (age>=60){
+// console.log("major");
+// }else if(age>=18){
+    // console.log("adult");
+// }else if(age>=18){
+
+// }
+
+
+// }else if{
+
+// }
+// todo:unit=250 by using else if
+let unit = 250;
+
+if (unit <= 100) {
+
+    console.log("Low Consumption");
+
+} else if (unit <= 200) {
+
+    console.log("Medium Consumption");
+
+} else if (unit <= 300) {
+
+    console.log("High Consumption");
+
+} else {
+
+    console.log("Very High Consumption");
+
+}
+//?switch case
+// let day=3;
+// switch(day){
+//     case 1:{
+//         console.log("sunday");
+//         break;
+//     }
+//     case 2:{
+//         console.log("monday");
+//         break;
+//     }
+//     case 3:{
+//         console.log("tuesday");
+//         break;
+//     }
+//     case 4:{
+//         console.log("wednesday");
+//         break;
+//     }
+//     case 5:{
+//         console.log("thursday");
+//         break;
+//     }
+//     case 6:{
+//         console.log("friday");
+//         break;
+//     }
+//     case 7:{
+//         console.log("saturday");
+//         break;
+//     }
+//     default:{
+//         console.log("enter day bertween 1-7");
+//     }
+
+// }
+//  let day=3;
+// switch(day){
+//     case 1:{
+//         console.log("sunday");
+//          break;
+//      }
+//     case 2:{
+//       console.log("monday");
+//        break;
+//      }
+//     case 3:{
+//         console.log("tuesday");
+//          break;
+//      }
+//      case 4:{
+//         console.log("wednesday");
+//         break;
+//     }
+//    case 5:{
+//         console.log("thursday");
+//        break;
+//    }
+//    case 6:{
+//       console.log("friday");
+//        break;
+//    }
+//    case 7:{
+//        console.log("saturday");
+//        break;
+//    }
+//    default:{
+//        console.log("enter day bertween 1-7");
+//    }
+
+// switch(day){
+//     case 1:
+//         case 7:{
+//         console.log("weekend day");
+//         break;
+//     }
+//     case 2:
+//         case 3:
+//             case 4: 
+//             case 5:{
+//         console.log("working day");
+//         break;
+//     }
+//     case 3:{
+//         console.log("tuesday");
+//         break;
+//     }
+//     case 4:{
+//          console.log("wednesday");
+//         break;
+//     }
+//     case 5:{
+//          console.log("thursday");
+//          break;
+//      }
+//     case 6:{
+//         console.log("friday");
+//          break;
+//     }
+//      case 7:{
+//         console.log("saturday");
+//         break;
+//     }
+//      default:{
+//         console.log("enter day bertween 1-7");
+//     }
+
+
+// //#loops
+// //?while
+// let i=0;
+// while(i<=10){
+// console.log(i);
+// i++;
+// }
+// //?do while
+// console.log("do while")
+// let j=0;
+// do{
+//     console.log(j);
+//     j++;
+
+// }while (j<=10);
+
+
+
+// // for loop
+// for(let i=0; i<=10; i++){
+//     console.log(i);
+// }
+// //reutrn 
+
+
+
+//!control flow
