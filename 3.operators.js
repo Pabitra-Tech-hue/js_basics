6102392012bce4cdc14bf4d35561e8712ce53dbf
pabitra dangi <pabitra-Tech-hue>
2026-06-22 14:02:56 +0545

    add: js operators

---
+++ 3.operators.js
@@ -0,0 +1,100 @@
+//!operators
+
+//#arithematic
+//+, -,*,/,**,%
+//console.log(12+5);
+// let a=5;
+// let b = 12;
+// console.log(a+b);
+// operator overloading
+// console.log(hello + world)
+//let sum = a+b;
+//console.log(sum);
+
+//?assignment
+//?=, +=, -=
+// let a = 10;
+//a= a+5;//a+=5
+// a=a+b;//a+=b;
+
+
+//?logical
+//?AND, OR,NOT
+// return Boolean
+// console.log(true&&true);
+// console.log(false&&true);
+// console.log(false||true);
+// console.log(false||false);
+
+//?comparison
+//? ==, ===,>,<,>=,<=,!=,!==
+//return bolean
+// console.log(10<10);
+// console.log(10<=10);
+// let c=10;
+// let d ="10";
+// console.log(c==d);
+// console.log(c===d);
+// console.log(c!==d);
+// console.log(c!===d);
+//unary operator
+// increment++
+// decrement--
+// let i=1;
+//?pre
+// console.log(++i);
+//post
+// console.log(i++);
+// console.log(i);
+
+
+
+//ternary operator
+//condition ?exp_1:exp_2
+// let age=17;
+// age>=18 ? console.log("can vote")  : console.log("cannot vote");
+
+
+//!type of
+// console.log(typeof canvote);
+// canvote=45;
+// console.log(typeof canvote==="string");
+// console.log(typeof 12);
+// console.log(typeof 1.452);
+// console.log(typeof null );
+
+
+
+//*type conversion
+//explicit conversion
+// string()
+// console.log(string(12));
+// console.log(number(123));
+// console.log(number(abc));
+// console.log(boolean("xyz"));
+//implicit conversion
+
+//?type coercion
+console.log(12 +"12");
+console.log(12 -"6");
+
+
+
+// truthy && falsy values
+//#falsy values:0, -0, null, undefined,false, nan -> false
+// console.log(boolean(0));
+// console.log(boolean(-0));
+// console.log(boolean(""));
+// let age=17;
+// // let canvote=age>=18 ? "can vote" :"cannot vote";
+// if (age){
+//     console.log("age is" ,age);
+
+// }else{
+//     console.log("age not found");
+// }
+
+
+
+int score = 50;
+score += 10;
