4f6d0e3cefc01b23729944097fcbf77d1adfb59f
pabitra dangi <pabitra-Tech-hue>
2026-06-23 07:32:18 +0545

    add:js hoisting

---
+++ 9.hoisting.js
@@ -0,0 +1,22 @@
+// *
+// console.log(x);
+// var x=20;
+// console.log(x);
+// //function declaration
+// hoist()
+// function hoist(){
+//     console.log("hoist");
+// }
+
+//function expression
+// a();
+// var a=function(){
+//     console.log("a");
+
+// };
+
+// console.log(b);
+// let b=10;
+console.log(c);
+const c=12;
+
