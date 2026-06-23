28bd63f0894ec28aa4dd55ce434084c3c5a42e27
pabitra dangi <pabitra-Tech-hue>
2026-06-23 08:32:09 +0545

    add:js git1

---
+++ 17.git1.js
@@ -0,0 +1,32 @@
+// !git and github
+// *git->vcs,tool
+// *github->
+// gitlab, bitbucket
+
+// *git commands
+// ?config
+// * git config --global --list
+// *git config --global user.name "pabitra dangi"
+// *git config --global user.email "pabitra-Tech-hue"
+// *git config --global init.defaultbranch "main"
+
+
+// !reposotory /repo
+// local->
+// remote->github ma backup rakhna host garna git hub use garxam github ma jun local repo lai host garna or yesko copy rakhna lagi repository folder  reate garxam tya pani git hub ma bhako repo lai nai hami remote repository bhanxam
+
+
+// ? initialize git repo
+// *git init  //initailized empty git repositroy
+
+
+
+// !git workflow
+// ?working directory  ->  staging area                      ->local repo
+//   cahnges               ready state                          commit/
+
+
+// *git status
+//*git add <title-path>(staging area/ready state)
+// *git commit -m "<commit_message"
+// * git push origin main(git dekhi github ma push garnalai)
