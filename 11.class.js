435aa88db69a1fef7ebf97ea8683f6ffe44d0b7c
pabitra dangi <pabitra-Tech-hue>
2026-06-23 07:35:39 +0545

    add: js class

---
+++ 11.class.js
@@ -0,0 +1,40 @@
+// //!class
+// class user{
+//     name;
+//     email;
+//     password;
+//     constructor(name,email,password){
+//         this.name=name;
+//         this.email=email;
+//         this.password=password;
+
+//     }
+// }
+// const john= new user("john doe","john@gmail.com","john123");
+// const john44= new user("john44 doe","john44@gmail.com","john123");
+// console.log(john);
+// console.log(john44);
+// console.log(john.passwrd);
+
+
+class car{
+    constructor(brand){
+        this.brand=brand;
+    }
+}
+const car1=new car("toyoto");
+console.log(car1.brand);
+
+
+
+class person{
+    constructor (name){
+        this.name=name;
+    }
+    
+}
+const person1=new person("ram");
+const person2=new person("hari");
+console.log(person1.name);
+console.log(person2.name);
+
