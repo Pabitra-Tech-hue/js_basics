560698df7290b865899cead50d0a3a4777d9cfec
pabitra dangi <pabitra-Tech-hue>
2026-06-23 14:10:15 +0545

    add:js git1

--- 17.git1.js
+++ 17.git1.js
@@ -40,3 +40,17 @@
 
 // *git merge <branch_name>
 //*git switch -c <branch name>
+
+
+function login(username, password) {
+  const storedUsername = "admin";
+  const storedPassword = "1234";
+
+  if (username === storedUsername && password === storedPassword) {
+    console.log("Login Successful");
+  } else {
+    console.log("Invalid Username or Password");
+  }
+}
+
+
