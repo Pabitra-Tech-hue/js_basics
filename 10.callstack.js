4917543688ba1f7f927b21b3ea660f80e8316368
pabitra dangi <pabitra-Tech-hue>
2026-06-23 07:33:46 +0545

    add:js callstack

---
+++ 10.callstack.js
@@ -0,0 +1,26 @@
+// function first(){
+//     console.log(first)
+// }
+// function second(){
+//     first()
+//     console.log(second)
+// }
+// second();
+
+ function e() {
+console.log("e")
+}
+e();
+
+// execution context
+
+// phase1:memory creation 
+// memory allocate
+// phase2:execution phase
+// memory initailization
+// code execute
+
+
+
+
+
